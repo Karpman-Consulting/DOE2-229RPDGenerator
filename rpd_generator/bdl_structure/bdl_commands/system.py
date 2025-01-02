39e6dd0a085215bc478c4b1ec5fde7f65abe1ab0
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2025-01-02 09:03:57 -0500

    remove todo

--- rpd_generator/bdl_structure/bdl_commands/system.py
+++ rpd_generator/bdl_structure/bdl_commands/system.py
@@ -516,7 +516,7 @@ class System(ParentNode):
             and heat_type
             in [
                 HeatingSystemOptions.FLUID_LOOP,
-                HeatingSystemOptions.NONE,  # TODO: No electric type prevents assigning temp_control
+                HeatingSystemOptions.NONE,
             ]
             and cool_type
             in [
